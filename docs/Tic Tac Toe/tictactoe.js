40811ce758fc67d8c31ffa341dbcad08cfb151f7
Mudduwe Jayasekara <tharinduj31@gmail.com>
2024-10-15 11:13:41 -0700

    Add tic tac toe game Canvas

---
+++ docs/Tic Tac Toe/tictactoe.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(() => {
+  window.addEventListener("load", (event) => {
+    // Canvas references
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+  });
+})();

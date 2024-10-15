5d86abde02f7f507d1e5e96eba2fba653b2492d9
Mudduwe Jayasekara <tharinduj31@gmail.com>
2024-10-15 14:27:29 -0700

    Initiate constants

--- docs/Tic Tac Toe/tictactoe.js
+++ docs/Tic Tac Toe/tictactoe.js
@@ -5,5 +5,23 @@
     // Canvas references
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
+
+     // UI references
+     const restartButton = document.querySelector("#restart");
+     const playerTurnText = document.querySelector("#turn-text");
+ 
+     // Constants
+     const CELLS_PER_AXIS = 3; // Tic-Tac-Toe is a 3x3 grid
+     const CELL_WIDTH = canvas.width / CELLS_PER_AXIS;
+     const CELL_HEIGHT = canvas.height / CELLS_PER_AXIS;
+     const EMPTY_CELL = "";
+     const PLAYER_X = "X";
+     const PLAYER_O = "O";
+
+
+
+
+
+
   });
 })();
